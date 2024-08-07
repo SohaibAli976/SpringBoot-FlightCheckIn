c49bd3953ed41e08f57bf4383984ef7c881e7cf3
SohaibAli976 <59691179+SohaibAli976@users.noreply.github.com>
2024-08-07 17:51:56 +0500

    Code OPtimization
    Code OPtimization

--- src/main/java/com/sohaib/flightcheckin/controllers/CheckInController.java
+++ src/main/java/com/sohaib/flightcheckin/controllers/CheckInController.java
@@ -6,6 +6,7 @@ import com.sohaib.flightcheckin.integration.dtos.ReservationUpdateRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,11 @@ public class CheckInController {
 
     @Autowired
     ReservationRestClient reservationRestClient;
+
+    @GetMapping
+    public String checkin() {
+        return "index";
+    }
 
     @RequestMapping("/showStartCheckin")
     public String showStartCheckIn()
--- src/main/java/com/sohaib/flightcheckin/integration/ReservationRestClientImpl.java
+++ src/main/java/com/sohaib/flightcheckin/integration/ReservationRestClientImpl.java
@@ -2,14 +2,17 @@ package com.sohaib.flightcheckin.integration;
 
 import com.sohaib.flightcheckin.integration.dtos.Reservation;
 import com.sohaib.flightcheckin.integration.dtos.ReservationUpdateRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ReservationRestClientImpl implements ReservationRestClient{
 
-    public static final String RESERVATION_REST_URL = "http://localhost:8080/flightReservation/reservations/";
-    public static final String RESERVATION_REST_URL_1 = "http://localhost:8080/flightReservation/reservations";
+    @Value("${com.sohaib.flightcheckin.reservationclient.url}")
+    public String RESERVATION_REST_URL; //= "http://localhost:8080/flightReservation/reservations/";
+    @Value("${com.sohaib.flightcheckin.reservationclient.url1}")
+    public  String RESERVATION_REST_URL_1; //= "http://localhost:8080/flightReservation/reservations";
 
     @Override
     public Reservation findReservation(Long id) {
@@ -20,6 +23,6 @@ public class ReservationRestClientImpl implements ReservationRestClient{
     @Override
     public Reservation updateReservation(ReservationUpdateRequest request) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.postForObject(RESERVATION_REST_URL_1,request,Reservation.class);
+         return restTemplate.postForObject(RESERVATION_REST_URL_1,request,Reservation.class);
     }
 }
